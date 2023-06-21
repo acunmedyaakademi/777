6ff2b140f0dbd4566ecdff63413f05d946f2bfce
cigdemkbk <101790567+Cigdemkabak@users.noreply.github.com>
2023-06-21 16:53:05 +0300

    şifre düzenlemeleri yapıldı

--- 777/Controllers/UserController.cs
+++ 777/Controllers/UserController.cs
@@ -179,6 +179,16 @@ namespace _777.Controllers
         }
 
 
+        private bool ContainsLowerCaseLetter(string password)
+        {
+            return password.Any(c => char.IsLower(c));
+        }
+
+        private bool ContainsUpperCaseLetter(string password)
+        {
+            return password.Any(c => char.IsUpper(c));
+        }
+
 
     }
 }
